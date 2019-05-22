3e2102f3038be32c85e98c2bd6a6023c63fae070
emkael <emkael@tlen.pl>
2019-05-23 01:21:23 +0200

    argparse support

--- deal-converter.py
+++ deal-converter.py
@@ -2,5 +2,17 @@ import argparse, sys
 
 from dealconvert import DealConverter
 
-converter = DealConverter(sys.argv[1])
-converter.output(sys.argv[2:])
+parser = argparse.ArgumentParser(
+    description='Universal converter for bridge deal formats',
+    formatter_class=argparse.RawTextHelpFormatter,
+    epilog='Supported formats: BER BHG BRI CDS CSV DGE DLM DUP PBN RZD.\n' + \
+    'Formats are auto-detected based on file extension.')
+parser.add_argument('input', metavar='INPUT_FILE',
+                    help='Input file path')
+parser.add_argument('output', metavar='OUTPUT_FILE', nargs='*',
+                    help='Output file path(s)')
+
+arguments = parser.parse_args()
+
+converter = DealConverter(arguments.input)
+converter.output(arguments.output)
