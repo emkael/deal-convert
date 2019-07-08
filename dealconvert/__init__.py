1dcac17da449fab9993523eb8bfe69ca73bcc726
emkael <emkael@tlen.pl>
2019-07-08 22:55:21 +0200

    Compatibility with interface renamed for API purposes Fixes #2

--- dealconvert/__init__.py
+++ dealconvert/__init__.py
@@ -5,12 +5,12 @@ class DealConverter(object):
         self.input = input_file
         self.formats = {}
         if input_file is not None:
-            self.parser = self._detect_format(self.input)
+            self.parser = self.detect_format(self.input)
 
     def output(self, output_files):
         deal_set = sorted(self.parser.parse(self.input), key=lambda d:d.number)
         for output in output_files:
-            self._detect_format(output).output(output, deal_set)
+            self.detect_format(output).output(output, deal_set)
 
     def detect_format(self, filename):
         for deal_format in globals()['formats'].__all__:
